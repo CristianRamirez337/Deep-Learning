ae26b9c698f06d348c607e74b04ba3e469068985
WinsteadZhu <55538690+WinsteadZhu@users.noreply.github.com>
2019-10-27 18:28:06 +0100

    Create Numpy_basics.py

---
+++ Numpy basics/Numpy_basics.py
@@ -0,0 +1,17 @@
+
+import math
+
+def basic_sigmoid(x):
+    """
+    Compute sigmoid of x.
+
+    Arguments:
+    x -- A scalar
+
+    Return:
+    s -- sigmoid(x)
+    """
+    
+    s = 1 / (1 + 1 / math.exp(x))
+    
+    return s
