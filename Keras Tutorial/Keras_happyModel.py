f157d11906a919a24d8fbfc0cb222b3382ec28a8
WinsteadZhu <55538690+WinsteadZhu@users.noreply.github.com>
2019-10-29 08:28:17 +0100

    Update Keras_happyModel.py

--- Keras Tutorial/Keras_happyModel.py
+++ Keras Tutorial/Keras_happyModel.py
@@ -1,3 +1,5 @@
+
+# Install packages
 import numpy as np
 from keras import layers
 from keras.layers import Input, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D
@@ -19,3 +21,109 @@ import matplotlib.pyplot as plt
 from matplotlib.pyplot import imshow
 
 %matplotlib inline
+
+
+
+
+# Normalize the data
+X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_dataset()
+
+# Normalize image vectors
+X_train = X_train_orig/255.
+X_test = X_test_orig/255.
+
+# Reshape
+Y_train = Y_train_orig.T
+Y_test = Y_test_orig.T
+
+print ("number of training examples = " + str(X_train.shape[0]))
+print ("number of test examples = " + str(X_test.shape[0]))
+print ("X_train shape: " + str(X_train.shape))
+print ("Y_train shape: " + str(Y_train.shape))
+print ("X_test shape: " + str(X_test.shape))
+print ("Y_test shape: " + str(Y_test.shape))
+
+
+
+
+# Build the model
+def HappyModel(input_shape):
+    """
+    Implementation of the HappyModel.
+    
+    Arguments:
+    input_shape -- shape of the images of the dataset
+
+    Returns:
+    model -- a Model() instance in Keras
+    """
+    
+    # Feel free to use the suggested outline to get started, and run through the whole
+    # program once. The come back also try out other network architectures as well. 
+    
+    # Define the input placeholder as a tensor with shape input_shape. Think of this as your input image!
+    X_input = Input(input_shape)
+    
+    # Zero-Padding: pads the border of X_input with zeroes
+    X = ZeroPadding2D((3, 3))(X_input)
+    
+    # CONV -> BN -> RELU Block applied to X
+    X = Conv2D(32, (7, 7), strides = (1, 1), name = 'conv0')(X)
+    X = BatchNormalization(axis = 3, name = 'bn0')(X)
+    X = Activation('relu')(X)
+    
+    # MAXPOOL
+    X = MaxPooling2D((2, 2), name = 'max_pool')(X)
+    
+    # FLATTEN X (means convert it to a vector) + FULLYCONNECTED
+    X = Flatten()(X)
+    X = Dense(1, activation = 'sigmoid', name = 'fc')(X)
+    
+    # Create model. This creates your Keras model instance, you'll use this instance to train/test the model.
+    model = Model(inputs = X_input, outputs = X, name = 'HappyModel')
+    
+    
+    return model
+  
+  
+  
+  
+# Create the model by calling the function above
+happyModel = HappyModel(X_train.shape[1:])
+  
+# Compile the model to configure the learning process
+happyModel.compile(optimizer = 'Adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+# Train the model
+happyModel.fit(x = X_train, y = Y_train, epochs = 20, batch_size = 50)
+
+# Test/evaluate the model
+preds = happyModel.evaluate(x = X_test, y = Y_test)
+
+print()
+print ("Loss = " + str(preds[0]))
+print ("Test Accuracy = " + str(preds[1]))
+
+
+
+
+
+# Test with your own image
+img_path = 'images/my_image.jpg'
+img = image.load_img(img_path, target_size=(64, 64))
+imshow(img)
+
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
+
+print(happyModel.predict(x))
+
+
+
+
+
+# Other useful functions
+happyModel.summary()
+plot_model(happyModel, to_file='HappyModel.png')
+SVG(model_to_dot(happyModel).create(prog='dot', format='svg'))
